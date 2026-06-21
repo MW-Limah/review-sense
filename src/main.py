6b9374816e97fff4480729268001f0ab6e059ba8
MW-Limah <cdrmw16@gmail.com>
2026-06-21 18:16:49 +0100

    feat: initialize fastapi backend structure and configure gemini sdk

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from review-sense!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.config.settings import settings
 
+app = FastAPI(
+    title='Smart Content Summarizer AI',
+    description='API para raspagem e sumarização de artigos usando Gemini 1.5 Flash',
+    version='1.0.0'
+)
 
-if __name__ == "__main__":
-    main()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins='[http://localhost:3000]',
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+@app.get('/')
+async def root():
+    return {
+        'status': 'outline',
+        'message': 'Smart Content Summarizer API is running sucessfully!'
+    }
