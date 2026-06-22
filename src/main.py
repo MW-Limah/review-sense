f631eddaf22e235ae839b4c5748517aca0f8f200
MW-Limah <cdrmw16@gmail.com>
2026-06-22 01:06:01 +0100

    fix: my portifolio url without '/'

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://delimadev.vercel.app/"
+        "https://delimadev.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
